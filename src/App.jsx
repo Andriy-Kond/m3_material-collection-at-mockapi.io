400ea280b431e4362058b6281e41b9526f55c26d
Andrii <andrii.kondratovich@gmail.com>
2024-07-31 11:45:13 +0300

    add: GlobalStyle.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { GlobalStyle } from "GlobalStyle";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
+
+export default App;
